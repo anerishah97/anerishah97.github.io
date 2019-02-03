b9edd8150e6b85ed671602b471436a2385c4108f
Aneri Shah <anerishah97@gmail.com>
2019-02-03 14:13:30 +0530

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -110,7 +110,7 @@
 		
         $(window).load(function(){
         $(".typing").typed({
-            strings: ["I live in the city of dreams, Mumbai"],    /* You can change the home section typing text from
+            strings: ["I live in Mumbai"],    /* You can change the home section typing text from
 	                                                                                            here and do not use "&" use "and" */
             typeSpeed: 100
           });
